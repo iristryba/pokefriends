d7bf8a438aa7102795143d7af096062ce048dd98
Iris Tryba <iristryba@gmail.com>
2022-07-10 21:22:52 +0200

    PokeFriends Logo from project source folders

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBox = ({ searchChange }) => {
     return (
-        <div className='p-2'>
+        <div className='pb-2'>
             <input 
                 type='search' 
                 placeholder='search by name or id'
--- src/components/TitleImage.js
+++ src/components/TitleImage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import titleImage from '../assets/Logo_PokeFriends.png'
 
 const TitleImage = ({isShrunk}) => {
     return (
         <div className={`max-w-md m-2 ${isShrunk ? "w-28" : "w-3/5"}`}>
-            <img src="https://fontmeme.com/permalink/220621/20c0bc187670ab6f68d281c3f55eadc4.png" alt="PokeFriends" border="0"></img>
+            <img src={ titleImage } alt="PokeFriends" border="0"></img>
+            {/* <img src="https://fontmeme.com/permalink/220621/20c0bc187670ab6f68d281c3f55eadc4.png" alt="PokeFriends" border="0"></img> */}
         </div> 
     )
 }
